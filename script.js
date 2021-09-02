bb5a3043c38ac66871bcf1cb4c9d9d1e1396ca71
MrBrightCosmos <89769112+MrBrightCosmos@users.noreply.github.com>
2021-09-02 14:49:03 +0530

    script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let startmenu = document.getElementsByClassName("startmenu")[0]
 
 taskbar.addEventListener("click", ()=>{
     console.log("clicked");
-    if(startmenu.style.bottom == "50px"){
+    if(startmenu.style.bottom == "60px"){
         startmenu.style.bottom = "-655px"
     }
     else{
-        startmenu.style.bottom = "50px"
+        startmenu.style.bottom = "60px"
     }
 })
